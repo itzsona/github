c43df8065d3a822ce1091a85d2733b7bd78a4053
Sonakshi Sareen <sonakshisareen1@gmail.com>
2024-07-13 01:44:41 +0530

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Sona on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
